1dc4ebe212ece7fbc886ba69d139b22aa0e6bc55
Robin Roy Julius <exo_duz@yahoo.com>
2016-07-21 21:31:52 -0700

    Cleaned up HTML on hero and included social accounts

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,23 +4,40 @@ class Hero extends React.Component {
 	render() {
 		return (
 			<section id="hero">
-				<div>
+				<div className="main-section">
 					<h1>Robin Roy Julius</h1>
-					<h2>
-						<span>UI / UX</span>
-						<span>Front End</span>
-						<span>Full Stack</span>
-						<span>Minimalist</span>
-					</h2>
+					
+					<div className="skills-and-interests">
+						<h2>UI / UX</h2>
+						<h2>Front End</h2>
+						<h2>Full Stack</h2>
+						<h2>Minimalist</h2>
+					</div>
 					<p className="more">
 						<a href="#">What I have worked on</a>
 						<a href="#">What I can do</a>
 						<a href="#">Say hi</a>
 					</p>
+
+					<div id="social-icons">
+						<a href="https://linkedin.com/in/robinjulius" target="_blank">
+							<i className="fa fa-linkedin"></i>
+						</a>
+						<a href="https://github.com/exoduz" target="_blank">
+							<i className="fa fa-github"></i>
+						</a>
+						<a href="https://twitter.com/exo_duz" target="_blank">
+							<i className="fa fa-twitter"></i>
+						</a>
+						<a href="https://instagram.com/exo_duz" target="_blank">
+							<i className="fa fa-instagram"></i>
+						</a>
+					</div>{/* #social-icons */}
 				</div>
+
 				<div className="scroll-for-more">
 					<a href="#"><i className="fa fa-long-arrow-down"></i> Would you like to know more?</a>
-				</div>
+				</div>{/* .scroll-for-more */}
 			</section>
 		)
 	}
