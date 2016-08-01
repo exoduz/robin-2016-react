e456b2acbf0c580bbfe591baef073f21538bad02
Robin Roy Julius <exo_duz@yahoo.com>
2016-08-01 16:51:46 -0700

    Updated coding language container for personal projects

--- src/components/Projects/PersonalProjects.js
+++ src/components/Projects/PersonalProjects.js
@@ -19,7 +19,7 @@ class PersonalProjects extends React.Component {
 								<img src="assets/img/robinjulius-2016-project-icon.png" alt="Robin Julius 2016 website icon" className="project-icon img-responsive" />
 								<h3>Robin's Website (mid 2016)</h3>
 								<p>Taking a minimalist and more open approach, I decided to rewrite my website to be a lot simpler.</p>
-								<p>
+								<p className="coding-language-container">
 									<span className="coding-language">ReactJS</span>
 									<span className="coding-language">jQuery</span>
 									<span className="coding-language">Webpack</span>
@@ -34,7 +34,7 @@ class PersonalProjects extends React.Component {
 								<img src="assets/img/robinjulius-2015-project-icon.png" alt="Robin Julius 2016 website icon" className="project-icon img-responsive" />
 								<h3>Robin's Website (late 2015)</h3>
 								<p>After studying ReactJS for a few weeks, I decided to take this opportunity to change my personal site from WordPress to ReactJS.</p>
-								<p>
+								<p className="coding-language-container">
 									<span className="coding-language">ReactJS</span>
 									<span className="coding-language">jQuery</span>
 									<span className="coding-language">Webpack</span>
@@ -50,7 +50,7 @@ class PersonalProjects extends React.Component {
 								<img src="assets/img/jerryyeoh-2016-project-icon.png" alt="Jerry Yeoh 2016 website icon" className="project-icon img-responsive" />
 								<h3>Jerry's Portfolio Website</h3>
 								<p>Originally this was written in vanilla PHP, but I was on a roll so I decided to also rewrite it in ReactJS.</p>
-								<p>
+								<p className="coding-language-container">
 									<span className="coding-language">ReactJS</span>
 									<span className="coding-language">jQuery</span>
 									<span className="coding-language">Webpack</span>
