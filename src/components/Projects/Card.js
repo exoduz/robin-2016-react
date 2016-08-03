b938b2ec49ae4d6b288b44de151b48ce66fa21f2
Robin Roy Julius <exo_duz@yahoo.com>
2016-08-02 20:58:58 -0700

    Recoded personal projects as JSON and output

---
+++ src/components/Projects/Card.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+class Card extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	/**
+	 _renderCodingLanguage
+	 Output coding languages
+	 @params language { string } coding language object
+	*/
+	_renderCodingLanguage(languages) {
+		if (Object.keys(languages).length === 0 && languages.constructor === Object) { //check if object empty
+			return;
+		} else {
+			return (
+				<p className="coding-language-container">
+					{ Object.keys(languages).map((id) => <span key={ id } className="coding-language">{ languages[id] }</span>)	}
+				</p>
+			)
+		}
+	}
+
+	/**
+	 _renderLinks
+	 Output links
+	 @params link { string } the link object
+	*/
+	_renderLinks(links) {
+		if (Object.keys(links).length === 0 && links.constructor === Object) { //check if object empty
+			return;
+		} else {
+			return (
+				<p>
+					{ Object.keys(links).map((id) => {
+						const link = links[id]['link'],
+							type = links[id]['type'],
+							icon = links[id]['icon'];
+
+						return (
+							<span key={ id }>
+								<a href={ link } target="_blank">
+									{ type === "github" ? "Download source on Github " : "Visit website " }
+									{ icon ? <i className={ icon }></i> : '' }
+								</a>
+								<br />
+							</span>
+						)
+					}) }
+				</p>
+			)
+		}
+	}	
+
+	render() {
+		console.log(this.props.data);
+		const { image, title, description, codingLanguage, links  } = this.props.data;
+		
+		return(
+			<div className="col-sm-4">
+				<div className="card">
+					<img src={ image } alt={ `${ title } image` } className="project-icon img-responsive" />
+					<h3>{ title }</h3>
+					<p>{ description }</p>
+					{ this._renderCodingLanguage(codingLanguage) }
+					{ this._renderLinks(links) }			
+				</div>{/* .card */}
+			</div>
+		)
+	}
+}
+
+Card.defaultProps = {
+	data: {}
+};
+
+Card.propTypes = {
+	data: React.PropTypes.object.isRequired
+}
+
+export default Card;
--- src/components/Projects/PersonalProjects.js
+++ src/components/Projects/PersonalProjects.js
@@ -1,66 +1,88 @@
 import React from 'react';
 
+import Card from './Card';
+
 class PersonalProjects extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
 	_data() {
 		const data = {
-
+			project1: {
+				image: "assets/img/robinjulius-2016-project-icon.png",
+				title: "Robin's Website (mid 2016)",
+				description: "Taking a minimalist and more open approach, I decided to rewrite my website to be a lot simpler.",
+				codingLanguage: {
+					codingLanguage1: "ReactJS",
+					codingLanguage2: "jQuery",
+					codingLanguage3: "Webpack"
+				},
+				links: {
+					link1: {
+						link: "https://github.com/exoduz/robin-2016-react",
+						type: "github",
+						icon: "fa fa-github"
+					}
+				}
+			},
+			project2: {
+				image: "assets/img/robinjulius-2015-project-icon.png",
+				title: "Robin's Website (late 2015)",
+				description: "After studying ReactJS for a few weeks, I decided to take this opportunity to change my personal site from WordPress to ReactJS.",
+				codingLanguage: {
+					codingLanguage1: "ReactJS",
+					codingLanguage2: "jQuery",
+					codingLanguage3: "Webpack"
+				},
+				links: {
+					link1: {
+						link: "https://robinjulius.com/work/robin-2015",
+						type: "website",
+						icon: "fa fa-external-link"
+					},
+					link2: {
+						link: "https://github.com/exoduz/robin-react",
+						type: "github",
+						icon: "fa fa-github"
+					}
+				}
+			},
+			project3: {
+				image: "assets/img/jerryyeoh-2016-project-icon.png",
+				title: "Jerry's Portfolio Website",
+				description: "Originally this was written in vanilla PHP, but I was on a roll so I decided to also rewrite it in ReactJS.",
+				codingLanguage: {
+					codingLanguage1: "ReactJS",
+					codingLanguage2: "jQuery",
+					codingLanguage3: "Webpack"
+				},
+				links: {
+					link1: {
+						link: "https://robinjulius.com/work/jerry-react",
+						type: "website",
+						icon: "fa fa-external-link"
+					},
+					link2: {
+						link: "https://github.com/exoduz/jerry-react",
+						type: "github",
+						icon: "fa fa-github"
+					}
+				}
+			}
 		}
 
 		return data;
 	}
 
 	render() {
+		const data = this._data();
+
 		return(
 			<section id="personal-projects">
 				<div className="container">
 					<div className="row">
-						<div className="col-sm-4">
-							<div className="card">
-								<img src="assets/img/robinjulius-2016-project-icon.png" alt="Robin Julius 2016 website icon" className="project-icon img-responsive" />
-								<h3>Robin's Website (mid 2016)</h3>
-								<p>Taking a minimalist and more open approach, I decided to rewrite my website to be a lot simpler.</p>
-								<p className="coding-language-container">
-									<span className="coding-language">ReactJS</span>
-									<span className="coding-language">jQuery</span>
-									<span className="coding-language">Webpack</span>
-								</p>
-								<p>
-									<a href="https://github.com/exoduz/robin-2016-react" target="_blank">Download source on Github <i className="fa fa-github"></i></a>
-								</p>
-							</div>{/* .card */}
-						</div>{/* .col-sm-4 */}
-						<div className="col-sm-4">
-							<div className="card">
-								<img src="assets/img/robinjulius-2015-project-icon.png" alt="Robin Julius 2016 website icon" className="project-icon img-responsive" />
-								<h3>Robin's Website (late 2015)</h3>
-								<p>After studying ReactJS for a few weeks, I decided to take this opportunity to change my personal site from WordPress to ReactJS.</p>
-								<p className="coding-language-container">
-									<span className="coding-language">ReactJS</span>
-									<span className="coding-language">jQuery</span>
-									<span className="coding-language">Webpack</span>
-								</p>
-								<p>
-									<a href="https://robinjulius.com/work/robin-2015" target="_blank">Visit website <i className="fa fa-external-link"></i></a><br />
-									<a href="https://github.com/exoduz/robin-react" target="_blank">Download source on Github <i className="fa fa-github"></i></a>
-								</p>
-							</div>{/* .card */}
-						</div>{/* .col-sm-4 */}
-						<div className="col-sm-4">
-							<div className="card">
-								<img src="assets/img/jerryyeoh-2016-project-icon.png" alt="Jerry Yeoh 2016 website icon" className="project-icon img-responsive" />
-								<h3>Jerry's Portfolio Website</h3>
-								<p>Originally this was written in vanilla PHP, but I was on a roll so I decided to also rewrite it in ReactJS.</p>
-								<p className="coding-language-container">
-									<span className="coding-language">ReactJS</span>
-									<span className="coding-language">jQuery</span>
-									<span className="coding-language">Webpack</span>
-								</p>
-								<p>
-									<a href="https://robinjulius.com/work/jerry-react" target="_blank">Visit website <i className="fa fa-external-link"></i></a><br />
-									<a href="https://github.com/exoduz/jerry-react" target="_blank">Download source on Github <i className="fa fa-github"></i></a>
-								</p>
-							</div>{/* .card */}
-						</div>{/* .col-sm-4 */}
+						{ Object.keys(data).map((entry) => <Card key={ entry } data={ data[entry] } />) }
 					</div>{/* .row */}
 				</div>{/* .container */}
 			</section>
